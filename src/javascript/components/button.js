aa8ffaaf4f52eca00735eb99241b4a2c715cc267
Mridula Udaya Shankar <mridu.sh92@gmail.com>
2018-05-12 14:00:06 -0700

    Add buttons for preference page

---
+++ src/javascript/components/button.js
@@ -0,0 +1,9 @@
+
+// <button type="button" class="btn btn-warning" data-food="110" >Food and drink</button>
+function preferenceButton(text,prefNum,pref) {
+   var buttonWarning = $('<button>');
+   buttonWarning.addClass('btn btn-warning');
+   buttonWarning.attr(`data-${pref}`,prefNum);
+   buttonWarning.text(text);
+}
+
