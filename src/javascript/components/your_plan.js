f1a849066263cd6d41db59cd7596678412d70471
Mridula Udaya Shankar <mridu.sh92@gmail.com>
2018-05-15 10:40:41 -0700

    Format the Eventbrite response data into component format

--- src/javascript/components/your_plan.js
+++ src/javascript/components/your_plan.js
@@ -8,17 +8,17 @@ function yourPlan(startDate,preferences,coordinates) {
       var planDiv = $('<div class="col-md-8" id="plan-view">');
 
       // Call data
-      var eventName = response.events[0].name.text;
-      var eventDescription = response.events[0].description.text;
+      var eventName = response.events[0].name.html;
+      var eventDescription = response.events[0].description.html;
       var eventURL = response.events[0].vanity_url;
       var startDate = response.events[0].start.local;
       var categoryID = response.events[0].category_id;
 
-      planDiv.append('****' + eventName + '****');
-      planDiv.append('****' + eventDescription + '****');
-      planDiv.append('****' + eventURL + '****');
-      planDiv.append('****' + startDate + '****');
-      planDiv.append('****' + categoryID + '****');
+      planDiv.append(eventName);
+      planDiv.append(eventDescription);
+      planDiv.append(eventURL);
+      planDiv.append(startDate);
+      planDiv.append(categoryID);
 
       $('#calendar-view').append(planDiv);
       buttonMaker('#button-view','Accept Plan',[{type: 'submit'},{class: 'btn btn-primary'}, {id: 'accept-plan'}]);
