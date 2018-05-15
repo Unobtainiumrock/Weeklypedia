1e3d628f05328b9a9935e4a9be481a45ea92669e
Unobtainiumrock <unobtainiumrock@gmail.com>
2018-05-14 23:56:12 -0700

    Test Niyati's ajax call function

--- src/javascript/components/your_plan.js
+++ src/javascript/components/your_plan.js
@@ -4,9 +4,21 @@ function yourPlan(startDate,preferences,coordinates) {
   console.log(preferences);
   console.log(coordinates);
   ajaxCall(startDate,preferences,coordinates);
-    /*render our plan for the day*/
+  // var API_KEY = "AECV6ECEY7DZIFOORUTH";
+  // var eventBriteURL = "https://cors-anywhere.herokuapp.com/https://www.eventbriteapi.com/v3/events/search";
+
+  // eventBriteURL += '?' + $.param({
+  //   'token': API_KEY,
+  //   'location.latitude': coordinates.latitude,
+  //   'location.longitude': coordinates.longitude,
+  //   'categories': preferences,
+  //   'start_date.range_start': startDate,
+  //   //'start_date.range_end':
+  //   'location.within': '10mi'
+  // })
+
     // $.ajax({
-  
+      
     // }).then(function() {
         // render plan to page 
           var planDiv = $('<div class="col-md-8" id="plan-view">');
